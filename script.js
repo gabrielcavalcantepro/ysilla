471c40ced4c0159d882db6f6ad5a619b906b01f0
gabrielcavalcantepro <gabrielcavalcantepro89@gmail.com>
2026-04-02 14:10:46 -0300

    v19

--- script.js
+++ script.js
@@ -252,3 +252,17 @@ if (span) {
 window.addEventListener("load", () => {
   ScrollTrigger.refresh();
 });
+
+
+
+//ANIMAÇÕES DE ENTRADA
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target); // não repete
+        }
+    });
+}, { threshold: 0.15 });
+
+document.querySelectorAll('.reveal').forEach(el => observer.observe(el));
